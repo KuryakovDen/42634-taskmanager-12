8b19e86d192074e3e54ce1413392ac1658b88f9c
Fifer2 <KuryakovDenis@yandex.ru>
2020-07-31 11:36:11 +0700

    feat: rendering for sorting

--- src/main.js
+++ src/main.js
@@ -93,5 +93,26 @@ const renderFiltersComponent = () => {
   `;
 };
 
+const renderBoardComponent = () => {
+  return `
+    <section class="board container"></section>
+  `;
+};
+
+const renderSortingComponent = () => {
+  return `
+    <div class="board__filter-list">
+      <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
+      <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+    </div>
+  `;
+};
+
 renderComponent(siteMainControl, renderMenuComponent());
 renderComponent(siteMainElement, renderFiltersComponent());
+renderComponent(siteMainElement, renderBoardComponent());
+
+const siteBoard = siteMainElement.querySelector(`.board`);
+
+renderComponent(siteBoard, renderSortingComponent());
