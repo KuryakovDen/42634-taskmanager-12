032a28aa7f3b9190d01fc645124af3965a4333a1
Fifer2 <KuryakovDenis@yandex.ru>
2020-07-31 11:52:51 +0700

    feat: render task component

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const COUNT_OF_TASKS = 8;
+
 const renderComponent = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
@@ -299,6 +301,54 @@ const renderEditTaskComponent = () => {
   `;
 };
 
+const renderTaskComponent = () => {
+  return `
+    <article class="card card--black">
+      <div class="card__form">
+        <div class="card__inner">
+          <div class="card__control">
+            <button type="button" class="card__btn card__btn--edit">
+              edit
+            </button>
+            <button type="button" class="card__btn card__btn--archive">
+              archive
+            </button>
+            <button
+              type="button"
+              class="card__btn card__btn--favorites card__btn--disabled"
+            >
+              favorites
+            </button>
+          </div>
+
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+
+          <div class="card__textarea-wrap">
+            <p class="card__text">Example task with default color.</p>
+          </div>
+
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                <div class="card__date-deadline">
+                  <p class="card__input-deadline-wrap">
+                    <span class="card__date">23 September</span>
+                    <span class="card__time">16:15</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  `;
+};
+
 renderComponent(siteMainControl, renderMenuComponent());
 renderComponent(siteMainElement, renderFiltersComponent());
 renderComponent(siteMainElement, renderBoardComponent());
@@ -311,7 +361,6 @@ renderComponent(siteBoard, renderBoardTasksComponent());
 
 const siteBoardTasks = siteMainElement.querySelector(`.board__tasks`);
 
-renderComponent(siteBoardTasks, renderEditTaskComponent());
-renderComponent(siteBoardTasks, renderEditTaskComponent());
-renderComponent(siteBoardTasks, renderEditTaskComponent());
-
+for (let i = 0; i < COUNT_OF_TASKS; i++) {
+  renderComponent(siteBoardTasks, renderTaskComponent());
+}
